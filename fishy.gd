935b7e77dde89d39acc92e07a9f2bc651ddcbb4e
Cuppit <xxhammerbladexx@gmail.com>
2024-09-05 00:19:39 -0500

    diag move, basic collision interactions

--- fishy.gd
+++ fishy.gd
@@ -1,12 +1,17 @@
 extends Area2D
 
+class_name Fishy # Important if things colliding with fishy need to know this is indeed a "fishy".
+
+var tile_size = 16 # TODO: Figure out global variables, and some authoritative way to determine tilesize (so it can 
+			   # change if tilemaps used change tile sizs
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print("The list of animations for a Fishy's animatedsprite2d is: ")
-	print($AnimatedSprite2D.get_animation())
-	$AnimatedSprite2D.play("red_whale")
-	print("It seems 'get_animation()' only reports the string name of the current ani, like so:", $AnimatedSprite2D.get_animation())
+	#position = position.snapped(Vector2.ONE * tile_size)
+	#print("The list of animations for a Fishy's animatedsprite2d is: ")
+	#print($AnimatedSprite2D.get_animation())
+	#$AnimatedSprite2D.play("red_whale")
+	#print("It seems 'get_animation()' only reports the string name of the current ani, like so:", $AnimatedSprite2D.get_animation())
 	
 	pass # Replace with function body.
 
--- player.gd
+++ player.gd
@@ -3,14 +3,20 @@ extends Area2D
 
 @onready var ray = $RayCast2D
 
-var animation_speed = 6
+var animation_speed = 9
 var moving = false
-
+var collider = null # utility variable for checking collisions with the ray.
+var fishy = load("res://fishy.tscn")
+var fishy_class = fishy.instantiate().get_class()
 var tile_size = 16
 var inputs = {"right": Vector2.RIGHT,
 			 "left": Vector2.LEFT,
 			 "up": Vector2.UP,
-			 "down": Vector2.DOWN}
+			 "down": Vector2.DOWN,
+			 "up_right": Vector2.UP+Vector2.RIGHT,
+			 "down_right": Vector2.DOWN+Vector2.RIGHT,
+			 "down_left": Vector2.DOWN+Vector2.LEFT,
+			 "up_left": Vector2.UP+Vector2.LEFT}
 
 
 # Called when the node enters the scene tree for the first time.
@@ -40,6 +46,10 @@ func move(dir):
 		moving = false
 	else:
 		print ("COLLISION DETECTED!")
+		collider = ray.get_collider()
+		if collider.get_class() == fishy_class:
+			print('You found the fishy!')
+		
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
